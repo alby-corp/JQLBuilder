4cd2551674ef2b707931c653cc946fffec2cd12e
Alberto Viezzi <aviezzi@outlook.com>
2024-01-13 11:16:02 +0100

    Fix Project and Version Tests

--- JQLBuilder.Types.Tests/Types/ProjectTests.Membership.cs
+++ JQLBuilder.Types.Tests/Types/ProjectTests.Membership.cs
@@ -1,25 +1,13 @@
 ﻿namespace JQLBuilder.Types.Tests.Types;
 
 using Constants;
+using Infrastructure;
+using JqlTypes;
 using Support;
 
 public partial class ProjectTests
 {
     #region In
-
-    [TestMethod]
-    public void Should_Parses_In_Params_When_Are_Heterogeneous()
-    {
-        var expected = $"""
-                        {Fields.Project} {Operators.In} ({ProjectId}, "{ProjectName}")
-                        """;
-
-        var actual = JqlBuilder.Query
-            .Where(f => f.Project.In(ProjectId, ProjectName))
-            .ToString();
-
-        Assert.AreEqual(expected, actual);
-    }
 
     [TestMethod]
     public void Should_Parses_In_Params_When_Are_Homogeneous()
@@ -32,30 +20,44 @@ public partial class ProjectTests
 
         Assert.AreEqual(expected, actual);
     }
-
+    
     [TestMethod]
-    public void Should_Parses_Not_In_Params_When_Are_Heterogeneous()
+    public void Should_Parses_In_Collection_When_Are_Homogeneous()
     {
-        var expected = $"""
-                        {Fields.Project} {Operators.In} ({ProjectId}, "{ProjectName}")
-                        """;
+        var expected = $"{Fields.Project} {Operators.In} ({ProjectId}, {ProjectId}, {ProjectId})";
+        
+        var filter = new JqlCollection<ProjectExpression> { ProjectId, ProjectId, ProjectId };
 
         var actual = JqlBuilder.Query
-            .Where(f => f.Project.In(ProjectId, ProjectName))
+            .Where(f => f.Project.In(filter))
             .ToString();
 
         Assert.AreEqual(expected, actual);
     }
-
+    
     [TestMethod]
-    public void Should_Parses_Not_In_Params_When_Are_Homogeneous()
+    public void Should_Parses_In_Params_When_Are_Heterogeneous()
     {
-        const string expected = $"""
-                                 {Fields.Project} {Operators.In} ("{ProjectName}", "{ProjectName}", "{ProjectName}")
-                                 """;
+        var expected = $"""
+                        {Fields.Project} {Operators.In} ({ProjectId}, "{ProjectName}", {ProjectId})
+                        """;
 
         var actual = JqlBuilder.Query
-            .Where(f => f.Project.In(ProjectName, ProjectName, ProjectName))
+            .Where(f => f.Project.In(ProjectId, ProjectName, ProjectId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [TestMethod]
+    public void Should_Parses_In_Collection_When_Are_Heterogeneous()
+    {
+        var expected = $"""{Fields.Project} {Operators.In} ({ProjectId}, "{ProjectName}", {ProjectId})""";
+        
+        var filter = new JqlCollection<ProjectExpression> { ProjectId, ProjectName, ProjectId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Project.In(filter))
             .ToString();
 
         Assert.AreEqual(expected, actual);
@@ -66,26 +68,26 @@ public partial class ProjectTests
     #region Not In
 
     [TestMethod]
-    public void Should_Parses_NotIn_Params_When_Are_Heterogeneous()
-    {
-        var expected = $"""
-                        {Fields.Project} {Operators.NotIn} ({ProjectId}, "{ProjectName}")
-                        """;
-
-        var actual = JqlBuilder.Query
-            .Where(f => f.Project.NotIn(ProjectId, ProjectName))
-            .ToString();
-
-        Assert.AreEqual(expected, actual);
-    }
-
-    [TestMethod]
     public void Should_Parses_NotIn_Params_When_Are_Homogeneous()
     {
         var expected = $"{Fields.Project} {Operators.NotIn} ({ProjectId}, {ProjectId}, {ProjectId})";
 
         var actual = JqlBuilder.Query
             .Where(f => f.Project.NotIn(ProjectId, ProjectId, ProjectId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [TestMethod]
+    public void Should_Parses_NotIn_Collection_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.Project} {Operators.NotIn} ({ProjectId}, {ProjectId}, {ProjectId})";
+        
+        var filter = new JqlCollection<ProjectExpression> { ProjectId, ProjectId, ProjectId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Project.NotIn(filter))
             .ToString();
 
         Assert.AreEqual(expected, actual);
@@ -104,20 +106,20 @@ public partial class ProjectTests
 
         Assert.AreEqual(expected, actual);
     }
-
+    
     [TestMethod]
-    public void Should_Parses_Not_NotIn_Params_When_Are_Homogeneous()
+    public void Should_Parses_NotIn_Collection_When_Are_Heterogeneous()
     {
-        const string expected = $"""
-                                 {Fields.Project} {Operators.NotIn} ("{ProjectName}", "{ProjectName}", "{ProjectName}")
-                                 """;
+        var expected = $"""{Fields.Project} {Operators.NotIn} ({ProjectId}, "{ProjectName}", {ProjectId})""";
+        
+        var filter = new JqlCollection<ProjectExpression> { ProjectId, ProjectName, ProjectId };
 
         var actual = JqlBuilder.Query
-            .Where(f => f.Project.NotIn(ProjectName, ProjectName, ProjectName))
+            .Where(f => f.Project.NotIn(filter))
             .ToString();
 
         Assert.AreEqual(expected, actual);
     }
-
+    
     #endregion
 }
--- JQLBuilder.Types.Tests/Types/VersionTests.Membership.cs
+++ JQLBuilder.Types.Tests/Types/VersionTests.Membership.cs
@@ -1,23 +1,13 @@
 ﻿namespace JQLBuilder.Types.Tests.Types;
 
 using Constants;
+using Infrastructure;
+using JqlTypes;
 using Support;
-using Functions = JQLBuilder.Functions;
-using FunctionsConstants = Constants.Functions;
 
 public partial class VersionTests
 {
-    [TestMethod]
-    public void Should_Parses_In_Params_When_Are_Heterogeneous()
-    {
-        var expected = $"""{Fields.FixVersion} {Operators.In} ({VersionId}, "{VersionName}", {FunctionsConstants.LatestReleased})""";
-
-        var actual = JqlBuilder.Query
-            .Where(f => f.Version.Fix.In(VersionId, VersionName, f.Version.Functions.LatestReleased))
-            .ToString();
-
-        Assert.AreEqual(expected, actual);
-    }
+    #region In
 
     [TestMethod]
     public void Should_Parses_In_Params_When_Are_Homogeneous()
@@ -30,28 +20,106 @@ public partial class VersionTests
 
         Assert.AreEqual(expected, actual);
     }
-
+    
     [TestMethod]
-    public void Should_Parses_Not_In_Params_When_Are_Heterogeneous()
+    public void Should_Parses_In_Collection_When_Are_Homogeneous()
     {
-        var expected = $"""{Fields.FixVersion} {Operators.NotIn} ({VersionId}, "{VersionName}", {FunctionsConstants.LatestReleased})""";
+        var expected = $"{Fields.FixVersion} {Operators.In} ({VersionId}, {VersionId}, {VersionId})";
+        
+        var filter = new JqlCollection<VersionExpression> { VersionId, VersionId, VersionId };
 
         var actual = JqlBuilder.Query
-            .Where(f => f.Version.Fix.NotIn(VersionId, VersionName, Functions.Version.LatestReleased))
+            .Where(f => f.Version.Fix.In(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [TestMethod]
+    public void Should_Parses_In_Params_When_Are_Heterogeneous()
+    {
+        var expected = $"""
+                        {Fields.FixVersion} {Operators.In} ({VersionId}, "{VersionName}", {VersionId})
+                        """;
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Version.Fix.In(VersionId, VersionName, VersionId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [TestMethod]
+    public void Should_Parses_In_Collection_When_Are_Heterogeneous()
+    {
+        var expected = $"""{Fields.FixVersion} {Operators.In} ({VersionId}, "{VersionName}", {VersionId})""";
+        
+        var filter = new JqlCollection<VersionExpression> { VersionId, VersionName, VersionId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Version.Fix.In(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    #endregion
+
+    #region Not In
+
+    [TestMethod]
+    public void Should_Parses_NotIn_Params_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.FixVersion} {Operators.NotIn} ({VersionId}, {VersionId}, {VersionId})";
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Version.Fix.NotIn(VersionId, VersionId, VersionId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [TestMethod]
+    public void Should_Parses_NotIn_Collection_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.FixVersion} {Operators.NotIn} ({VersionId}, {VersionId}, {VersionId})";
+        
+        var filter = new JqlCollection<VersionExpression> { VersionId, VersionId, VersionId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Version.Fix.NotIn(filter))
             .ToString();
 
         Assert.AreEqual(expected, actual);
     }
 
     [TestMethod]
-    public void Should_Parses_Not_In_Params_When_Are_Homogeneous()
+    public void Should_Parses_Not_NotIn_Params_When_Are_Heterogeneous()
     {
-        const string expected = $"""{Fields.FixVersion} {Operators.NotIn} ("{VersionName}", "{VersionName}", "{VersionName}")""";
+        var expected = $"""
+                        {Fields.FixVersion} {Operators.NotIn} ({VersionId}, "{VersionName}")
+                        """;
 
         var actual = JqlBuilder.Query
-            .Where(f => f.Version.Fix.NotIn(VersionName, VersionName, VersionName))
+            .Where(f => f.Version.Fix.NotIn(VersionId, VersionName))
             .ToString();
 
         Assert.AreEqual(expected, actual);
     }
+    
+    [TestMethod]
+    public void Should_Parses_NotIn_Collection_When_Are_Heterogeneous()
+    {
+        var expected = $"""{Fields.FixVersion} {Operators.NotIn} ({VersionId}, "{VersionName}", {VersionId})""";
+        
+        var filter = new JqlCollection<VersionExpression> { VersionId, VersionName, VersionId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Version.Fix.NotIn(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+    
+    #endregion
 }
