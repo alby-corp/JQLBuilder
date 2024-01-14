426a64723c377cd7b43b08b854891a1a7cfe59f9
Alberto Viezzi <aviezzi@outlook.com>
2024-01-14 23:53:05 +0100

    Add Parent

---
+++ JQLBuilder.Types.Tests/Types/ParentTests.Equality.cs
@@ -0,0 +1,58 @@
+﻿namespace JQLBuilder.Types.Tests.Types;
+
+using Constants;
+
+public partial class ParentTests
+{
+    [TestMethod]
+    public void Should_Parses_Equals_Expression()
+    {
+        const string expected = $"""
+                                 {Fields.Parent} {Operators.Equals} "{ParentName}"
+                                 """;
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent == ParentName)
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_Not_Equals_Expression()
+    {
+        var expected = $"{Fields.Parent} {Operators.NotEquals} {ParentId}";
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent != ParentId)
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_Equals_Expression_Reverse()
+    {
+        const string expected = $"""
+                                 {Fields.Parent} {Operators.Equals} "{ParentName}"
+                                 """;
+
+        var actual = JqlBuilder.Query
+            .Where(f => ParentName == f.Parent)
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_Not_Equals_Expression_Reverse()
+    {
+        var expected = $"{Fields.Parent} {Operators.NotEquals} {ParentId}";
+
+        var actual = JqlBuilder.Query
+            .Where(f => ParentId != f.Parent)
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ JQLBuilder.Types.Tests/Types/ParentTests.Membership.cs
@@ -0,0 +1,125 @@
+﻿namespace JQLBuilder.Types.Tests.Types;
+
+using Constants;
+using Infrastructure;
+using JqlTypes;
+using Support;
+
+public partial class ParentTests
+{
+    #region In
+
+    [TestMethod]
+    public void Should_Parses_In_Params_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.Parent} {Operators.In} ({ParentId}, {ParentId}, {ParentId})";
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.In(ParentId, ParentId, ParentId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_In_Collection_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.Parent} {Operators.In} ({ParentId}, {ParentId}, {ParentId})";
+
+        var filter = new JqlCollection<ParentExpression> { ParentId, ParentId, ParentId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.In(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_In_Params_When_Are_Heterogeneous()
+    {
+        var expected = $"""
+                        {Fields.Parent} {Operators.In} ({ParentId}, "{ParentName}", {ParentId})
+                        """;
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.In(ParentId, ParentName, ParentId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_In_Collection_When_Are_Heterogeneous()
+    {
+        var expected = $"""{Fields.Parent} {Operators.In} ({ParentId}, "{ParentName}", {ParentId})""";
+
+        var filter = new JqlCollection<ParentExpression> { ParentId, ParentName, ParentId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.In(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    #endregion
+
+    #region Not In
+
+    [TestMethod]
+    public void Should_Parses_NotIn_Params_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.Parent} {Operators.NotIn} ({ParentId}, {ParentId}, {ParentId})";
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.NotIn(ParentId, ParentId, ParentId))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_NotIn_Collection_When_Are_Homogeneous()
+    {
+        var expected = $"{Fields.Parent} {Operators.NotIn} ({ParentId}, {ParentId}, {ParentId})";
+
+        var filter = new JqlCollection<ParentExpression> { ParentId, ParentId, ParentId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.NotIn(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_Not_NotIn_Params_When_Are_Heterogeneous()
+    {
+        var expected = $"""
+                        {Fields.Parent} {Operators.NotIn} ({ParentId}, "{ParentName}")
+                        """;
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.NotIn(ParentId, ParentName))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Should_Parses_NotIn_Collection_When_Are_Heterogeneous()
+    {
+        var expected = $"""{Fields.Parent} {Operators.NotIn} ({ParentId}, "{ParentName}", {ParentId})""";
+
+        var filter = new JqlCollection<ParentExpression> { ParentId, ParentName, ParentId };
+
+        var actual = JqlBuilder.Query
+            .Where(f => f.Parent.NotIn(filter))
+            .ToString();
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    #endregion
+}
