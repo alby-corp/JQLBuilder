7a7516898a8f09d71278d5ab1d336a7d28763cd4
roald-di <4057806+roald-di@users.noreply.github.com>
2024-02-07 08:58:26 +0100

    Improve TimeSpan to JqlDuration conversion

--- JQLBuilder/Infrastructure/TimeOffset.cs
+++ JQLBuilder/Infrastructure/TimeOffset.cs
@@ -1,3 +1,7 @@
 ﻿namespace JQLBuilder.Infrastructure;
 
-public record TimeOffset(int Years, int Months, int Weeks, int Days, int Hours, int Minutes);
+public record TimeOffset(int Years, int Months, int Weeks, int Days, int Hours, int Minutes)
+{
+    public static object FromTimeSpan(TimeSpan value)
+        => new TimeOffset(0, 0, 0, value.Days, value.Hours, value.Minutes);
+}
